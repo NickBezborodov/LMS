2822cbf1a0c61d8d0499a79856504f4a33ad6542
NickBezborodov <benlada1488@mail.ru>
2026-06-02 15:56:51 +0300

    Добавил логи.

--- src/main/java/com/example/lms/service/ScheduleCleanUpService.java
+++ src/main/java/com/example/lms/service/ScheduleCleanUpService.java
@@ -3,6 +3,7 @@ package com.example.lms.service;
 import com.example.lms.dao.ScheduleRepository;
 import com.example.lms.model.Schedule;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -10,6 +11,7 @@ import org.springframework.transaction.annotation.Transactional;
 import java.time.LocalDateTime;
 import java.util.List;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class ScheduleCleanUpService {
@@ -21,5 +23,6 @@ public class ScheduleCleanUpService {
         LocalDateTime oneYearAgo = LocalDateTime.now().minusYears(1);
         List<Schedule> oldSchedules = scheduleRepository.findOlderThanOneYear(oneYearAgo);
         scheduleRepository.deleteAll(oldSchedules);
+        log.info("Удалено {} старых расписаний", oldSchedules.size());
     }
 }
